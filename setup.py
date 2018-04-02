bc1270899e752a38c289aae0abe1fd41f3b5cfd8
Lucas Pollice <newuser@example.com>
2018-04-01 20:52:38 -0400

    It should actually work now ;)

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 long_description = open('README.md', 'r').read()
 setup(
-    name='LetterBitmapDictionary',
+    name='lbdict',
     description='A class for storing the pixel values of letters.',
     long_description=long_description,
-    version='0.1.2',
+    version='0.1.0',
     url='https://github.com/Jugbot/Letter-Bitmap-Dictionary',
     keywords='bitmap font dictionary',
     author='Jugbot',
     author_email='lucasp465@gmail.com',
-    install_requires=['pillow']
+    install_requires=['pillow'],
+    py_modules=['lbdict']
 )
